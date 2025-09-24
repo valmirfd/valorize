afb857c1edf7fa0636735cfd8de9dd79c9648098
Valmir Dornas <fernandocursos2021@gmail.com>
2025-09-24 11:36:55 -0300

    Model Basic

--- app/Database/Migrations/2025-09-24-135334_CreateIgrejas.php
+++ app/Database/Migrations/2025-09-24-135334_CreateIgrejas.php
@@ -8,11 +8,127 @@ class CreateIgrejas extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+
+            'id'          => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'user_id'          => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'null'           => false,
+            ],
+            'nome'       => [
+                'type'       => 'VARCHAR',
+                'constraint' => '128',
+            ],
+            'telefone' => [
+                'type'                  => 'VARCHAR',
+                'constraint'            => '20',
+                'null'                  => true,
+                'default'               => null,
+            ],
+            'cnpj'       => [
+                'type'       => 'VARCHAR',
+                'constraint' => '30',
+                'null'       => true,
+                'default'    => null,
+            ],
+            'codigo'       => [
+                'type'       => 'VARCHAR',
+                'constraint' => '128',
+            ],
+            'situacao'       => [
+                'type'       => 'ENUM',
+                'constraint' => ['sede', 'filial'],
+            ],
+            'cep'       => [
+                'type'       => 'VARCHAR',
+                'constraint' => '30',
+            ],
+            'rua'       => [
+                'type'       => 'VARCHAR',
+                'constraint' => '140',
+            ],
+            'numero'       => [
+                'type'       => 'VARCHAR',
+                'constraint' => '50',
+                'null'       => true,
+            ],
+            'bairro'       => [
+                'type'       => 'VARCHAR',
+                'constraint' => '140',
+            ],
+            'cidade'       => [
+                'type'       => 'VARCHAR',
+                'constraint' => '140',
+            ],
+            'estado'       => [
+                'type'       => 'VARCHAR',
+                'constraint' => '50',
+            ],
+            'superintendente_id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'null'           => true,
+                'default'        => null,
+            ],
+            'titular_id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'null'           => true,
+                'default'        => null,
+            ],
+            'is_sede'       => [
+                'type'       => 'BOOLEAN',
+                'null' => false,
+                'default' => false,
+            ],
+            'descricao'       => [
+                'type'       => 'LONGTEXT',
+            ],
+            'created_at'       => [
+                'type'       => 'DATETIME',
+                'null'       => true,
+                'default'    => null,
+            ],
+            'updated_at'       => [
+                'type'       => 'DATETIME',
+                'null'       => true,
+                'default'    => null,
+            ],
+            'deleted_at'       => [
+                'type'       => 'DATETIME',
+                'null'       => true,
+                'default'    => null,
+            ],
+            'ativo'       => [
+                'type'       => 'BOOLEAN',
+                'null' => false,
+                'default' => true,
+            ],
+        ]);
+
+
+        $this->forge->addKey('id', true); // primary key        
+        $this->forge->addKey('nome'); 
+        $this->forge->addKey('bairro'); 
+        $this->forge->addKey('cidade'); 
+        $this->forge->addKey('estado'); 
+
+        $this->forge->addForeignKey('user_id', 'users', 'id', 'CASCADE', 'CASCADE');
+
+        $this->forge->createTable('igrejas');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('igrejas');
     }
 }
---
+++ app/Models/Basic/AppModel.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Models\Basic;
+
+use CodeIgniter\Model;
+
+abstract class AppModel extends Model
+{
+
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+
+    protected bool $allowEmptyInserts = false;
+    protected bool $updateOnlyChanged = false;
+
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = ['escapeData'];
+    protected $beforeUpdate   = ['escapeData'];
+
+    protected function escapeData(array $data): array
+    {
+        return esc($data);
+    }
+
+    protected function setUserId(array $data): array
+    {
+        $data['data']['user_id'] = auth()->id();
+        return $data;
+    }
+
+    protected function setCode(array $data): array
+    {
+        do {
+            $codigo = rand(10000000, 99999999);
+
+            $result = $this->select('codigo')->where('codigo', $codigo)->countAllResults();
+        } while ($result > 0);
+
+        $data['data']['codigo'] = $codigo;
+
+        return $data;
+    }
+
+    public function whereUser(): self
+    {
+        $this->where("{$this->table}.user_id", auth()->id());
+        return $this;
+    }
+}
